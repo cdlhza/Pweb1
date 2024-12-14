98e7cbe61a34fce414e70a068a46c4a29224fab7
cdlhza <cdl.hza@skiff.com>
2024-12-14 14:30:05 -0600

    Agregar url frontend render y home del api

--- backend/src/models/products.models.js
+++ backend/src/models/products.models.js
@@ -1,33 +1,43 @@
 import mongoose from "mongoose";
-const productsSchema =new mongoose.Schema(
-    {
-    name:{
-        type: String,
-        required: true
-    },
-    price:{
-        type: Number,
-        default: 0.0,
-        required: true
-    },
-    year:{
-        type: Number,
-        default:2024,
-        required: true,
-    },
-    image:{
-        type: String,
-        required: true
-    },
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
 
-},{
-   timestamps: true 
-}
-);// fin de productosSchema
+const productsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      default: 0.0,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1, // Nueva propiedad
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true, // Nueva propiedad
+    },
+    sizes: {
+      type: [String], // Nueva propiedad: Arreglo de tallas (Ejemplo: ["S", "M", "L"])
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Esto mantiene automáticamente "createdAt" y "updatedAt".
+  }
+);
 
-export default mongoose.model('Prodcuts', productsSchema);
+export default mongoose.model("Products", productsSchema);
